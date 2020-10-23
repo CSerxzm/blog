54a597710625095d9a3358b8feac97bc924c1275
CSerxzm <3052720966@qq.com>
2020-10-23 17:10:41 +0800

    添加加密文件

--- src/test/java/com/xzm/BlogApplicationTests.java
+++ src/test/java/com/xzm/BlogApplicationTests.java
@@ -8,11 +8,13 @@ import com.xzm.dao.BlogMapper;
 import com.xzm.service.BlogService;
 import com.xzm.util.MD5Utils;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.cache.Cache;
@@ -25,6 +27,9 @@ import java.util.Map;
 @SpringBootTest
 public class BlogApplicationTests {
 
+	@Autowired
+	private StringEncryptor encryptor;
+
 	@Test
 	public  void test02(){
 		String password = MD5Utils.code("123456");
@@ -32,6 +37,14 @@ public class BlogApplicationTests {
 		/*
 		5f4dcc3b5aa765d61d8327deb882cf99
 		 */
+	}
+
+	@Test
+	public void test() {
+		System.out.println("username:" + encryptor.encrypt("123456"));
+		System.out.println("password:" + encryptor.encrypt("123456"));
+		System.out.println("emailpassword:" + encryptor.encrypt("123456"));
+
 	}
 
 }
